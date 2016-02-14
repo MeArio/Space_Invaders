c75df7121e12ecfd183c278cafba09ebddba13e9
MeArio <dorindovleac@gmail.com>
2016-02-14 02:06:59 +0200

    fixed some glitches, but it still doesn't as intended

--- assets/objects/enemy.lua
+++ assets/objects/enemy.lua
@@ -14,16 +14,16 @@ end
 
 function Enemy:create()
     if #self.enemy_table == 0 then
-        table.insert(self.enemy_table, {x = self.x, alive = true})
+        table.insert(self.enemy_table, {x = self.x, y = self.y, alive = true})
     else
-        table.insert(self.enemy_table, {x = self.enemy_table[#self.enemy_table].x + self.spacing, alive = true})
+        table.insert(self.enemy_table, {x = self.enemy_table[#self.enemy_table].x + self.spacing, y = self.y, alive = true})
     end
 end
 
 function Enemy:checkCollision()
     for i, enemy in ipairs(self.enemy_table) do
         if enemy.x + self.width >= love.graphics.getWidth() then
-            self.y = self.y + self.spacing
+            enemy.y = enemy.y + self.spacing
             enemy.x = self.x
         end
     end
@@ -31,11 +31,8 @@ end
 
 function Enemy:update(dt)
     for i, enemy in ipairs(self.enemy_table) do
-        prevX = enemy.x
-        if i ~= 1 then
-            enemy.x = prevX + self.spacing + self.speed * dt
-            enemy.y = self.y
-        end
+            enemy.x = enemy.x + self.speed * dt
+            --enemy.y = self.y
     end
 end
 
@@ -43,10 +40,10 @@ function Enemy:draw()
     for i, enemy in ipairs(self.enemy_table) do
         if enemy.alive then
             love.graphics.setColor(0, 255, 0)
-            love.graphics.rectangle("fill", enemy.x, self.y, self.width, self.height)
+            love.graphics.rectangle("fill", enemy.x, enemy.y, self.width, self.height)
         end
     end
 end
 
 return Enemy
---Enemy:update(), Enemy:destroy() --> Compensate for spacing Enemy:draw()
+--fix y axis
