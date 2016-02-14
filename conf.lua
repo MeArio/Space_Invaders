da68146564a4341857292c76a9e5d9d79ce481e1
MeArio <dorindovleac@gmail.com>
2016-02-14 02:05:56 +0200

    changed the name of the window title to Space_Invaders

--- conf.lua
+++ conf.lua
@@ -2,6 +2,6 @@ function love.conf(t)
     t.window.width = 600
     t.window.height = 700
     t.version = "0.10.0"
-    t.window.title = "Hangman"
+    t.window.title = "Space_Invaders"
     t.window.resizable = false
 end
